9ef87a110ceb7b78ef2546d28bb634e249ea2042
RitaPylypaka <ritapylypaka@gmail.com>
2023-11-06 18:50:25 +0200

    Add project files.

---
+++ UnitTest5.5/UnitTest5.5.cpp
@@ -0,0 +1,20 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../пр5.5/пр5.5.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest55
+{
+	TEST_CLASS(UnitTest55)
+	{
+	public:
+
+		TEST_METHOD(TestMethod1)
+		{
+			Assert::AreEqual(1, a(1));
+			Assert::AreEqual(22, a(2));
+		}
+	};
+}
+
---
+++ пр5.5/пр5.5.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int a(int n) {
+    if (n > 1) {
+        return 3 * 2 * (n - 1) + 8 * 2 * a(n - 1);
+    }
+    else {
+        if (n == 1)
+            return 1;
+        else
+            return 0;
+    }
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    cout << "a(n) = " << a(n) << endl;
+
+    return 0;
+}
